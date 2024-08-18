e67347d4163f44a3e8a36785627daf04b34b2bbc
GroundPilot <121142736+hank8884@users.noreply.github.com>
2024-08-18 23:35:54 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -6,6 +6,8 @@
 # Source code repository: https://github.com/openwrt/openwrt / Branch: main
 #========================================================================================================================
 
+is_wsl2op=$1
+
 # ------------------------------- Main source started -------------------------------
 #
 # Add the default password for the 'root' user（Change the empty password to 'password'）
@@ -16,8 +18,61 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 #
+#
+#
+# Add Theme
+rm -rf ./package/custom_packages
+mkdir -p ./package/custom_packages
+git clone https://github.com/jerrykuku/luci-theme-argon.git ./package/custom_packages/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/custom_packages/luci-app-argon-config
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/custom_packages/luci-app-adguardhome
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 ./package/custom_packages/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata ./package/custom_packages/v2ray-geodata
+
+# if [ ! -d "./package/lean/luci-app-argon-config" ]; then git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/luci-app-argon-config;   else cd ./package/lean/luci-app-argon-config; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
+# if [ ! -d "./package/lean/luci-app-adguardhome" ]; then git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/lean/luci-app-adguardhome;   else cd ./package/lean/luci-app-adguardhome; git stash; git stash drop; git pull; cd ..; cd ..; cd ..; fi;
+# git clone https://github.com/jerrykuku/lua-maxminddb.git
+# git clone https://github.com/jerrykuku/luci-app-vssr.git
+# git clone https://github.com/lisaac/luci-app-dockerman.git
+
+# Reset drive type
+# sed -i 's/(dmesg | grep .*/{a}${b}${c}${d}${e}${f}/g' package/lean/autocore/files/x86/autocore
+# sed -i '/h=${g}.*/d' package/lean/autocore/files/x86/autocore
+# sed -i 's/echo $h/echo $g/g' package/lean/autocore/files/x86/autocore
+
+# Close running yards
+# sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
+
+if [ ! -n "$is_wsl2op" ]; then
+    # Add default login background
+    cp -r $GITHUB_WORKSPACE/source/video/* ./package/custom_packages/luci-theme-argon/htdocs/luci-static/argon/background/
+    cp -r $GITHUB_WORKSPACE/source/img/* ./package/custom_packages/luci-theme-argon/htdocs/luci-static/argon/background/
+
+    # Inject download package
+    mkdir -p $GITHUB_WORKSPACE/openwrt/dl
+    cp -r $GITHUB_WORKSPACE/library/* $GITHUB_WORKSPACE/openwrt/dl/
+
+    # Fixed qmi_wwan_f complie error
+    # cp -r $GITHUB_WORKSPACE/patches/qmi_wwan_f.c $GITHUB_WORKSPACE/openwrt/package/wwan/driver/fibocom_QMI_WWAN/src/qmi_wwan_f.c
+
+else
+    # Add default login background
+    cp -r /home/$USER/OpenWrtAction/source/video/* ./package/custom_packages/luci-theme-argon/htdocs/luci-static/argon/background/
+    cp -r /home/$USER/OpenWrtAction/source/img/* ./package/custom_packages/luci-theme-argon/htdocs/luci-static/argon/background/
+
+    # Inject download package
+    mkdir -p dl
+    cp -r /home/$USER/OpenWrtAction/library/* dl/
+
+    # Fixed qmi_wwan_f complie error
+    # cp -r ../OpenWrtAction/patches/qmi_wwan_f.c ./package/wwan/driver/fibocom_QMI_WWAN/src/qmi_wwan_f.c
+fi
+
+# Diy
+# rm -rf ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# wget -P ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status https://github.com/smallprogram/OpenWrtAction/raw/main/source/openwrtfile/index.htm
 # ------------------------------- Main source ends -------------------------------
 
 # ------------------------------- Other started -------------------------------
